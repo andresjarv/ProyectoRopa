5c18b43cc254bae139d358186bda3329891a3b90
andresjarv <andresjarv95@gmail.com>
2025-05-21 22:18:31 -0500

    cambio en el program.cs del front

--- ROPA.Frontend/Program.cs
+++ ROPA.Frontend/Program.cs
@@ -10,10 +10,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 // Agregar la configuración de HttpClient para consumir APIs
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var urlLocal = "https://localhost:7593";
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(urlLocal) });
 // Registrar los servicios que consumen las APIs
 builder.Services.AddScoped<IProductoService, ProductoService>();
-builder.Services.AddScoped<IClienteService, ClienteService>();
+//builder.Services.AddScoped<IClienteService, ClienteService>();
 
 await builder.Build().RunAsync();
